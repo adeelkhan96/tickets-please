2517299c052b62d51af30cf18c6e057fff9fb5d2
Adeel Khan <adeeljadoon947@gmail.com>
2026-03-29 22:50:58 +0200

    Added api.php support and API test route

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * The path to the "home" route for your application.
+     *
+     * Typically, users are redirected here after login.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     */
+    public function boot(): void
+    {
+        $this->routes(function () {
+            // API routes
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+
+            // Web routes
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        });
+    }
+}
